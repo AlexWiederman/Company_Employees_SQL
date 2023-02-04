d6eb1af1ce41ae177c8ba88262fff9382fe6bbfa
Alex W <amwiederman15@gmail.com>
2023-02-04 15:28:40 -0500

    got a couple more queries to work out

--- index.js
+++ index.js
@@ -23,24 +23,24 @@ function mainMenu() {
             type: 'list',
         name: 'main',
         message: 'What would you like to do?',
-        choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View all Departments', 'Add Department','Quit'],
+        choices: ['View All Employees', 'Add an Employee', 'Update an Employee Role', 'View All Roles', 'Add a Role', 'View all Departments', 'Add a Department','Quit'],
         
         },
     ])
 .then((data) => {
     if (data.main == 'View All Employees') {
         viewAllEmployees()
-    } else if (data.main == 'Add Employee') {
+    } else if (data.main == 'Add an Employee') {
         addEmployees()
-    } else if (data.main == 'Update Employee Role') {
+    } else if (data.main == 'Update an Employee Role') {
         updateEmployeeRole()
     } else if (data.main == 'View All Roles') {
         viewAllRoles()
-    } else if (data.main == 'Add Role') {
+    } else if (data.main == 'Add a Role') {
         addRole()
     } else if (data.main == 'View all Departments') {
         allDepartments()
-    } else if (data.main == 'Add Department') {
+    } else if (data.main == 'Add a Department') {
         addDepartment()
     }else if (data.main == 'Quit') {
         return
@@ -56,6 +56,18 @@ function viewAllEmployees() {
   });
 };
 
+function addEmployees() {
+
+}
+
+function updateEmployeeRole() {
+
+}
+
+function addRole() {
+
+}
+
 function viewAllRoles() {
     const sql = `SELECT role.id,title,salary,department.name as department FROM role JOIN department ON department.id = role.department_id;`;
   db.query(sql, function (err, rows) {
@@ -70,6 +82,24 @@ function allDepartments() {
     console.table(rows);
     mainMenu()
   });
+}
+
+function addDepartment() {
+    inquirer
+    .prompt([
+        {
+        type: 'input',
+        name: 'main',
+        message: 'Enter the name of the new department.',
+        },
+    ])
+    .then((data) => {
+        const sql = `INSERT INTO department (name) VALUES ("${data.main}");`
+        db.query(sql, function (err, rows) {
+            console.log(`${data.main} Department Added`)
+            mainMenu()
+          });
+    })
 }
 
 console.log('--------Employee Manager--------')
