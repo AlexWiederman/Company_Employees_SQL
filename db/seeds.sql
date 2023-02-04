f16c9bf08537233eed180c79c7926f9736b787c8
Alex W <amwiederman15@gmail.com>
2023-02-04 06:46:49 -0500

    first commit with all files created

---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+INSERT INTO department (name)
+VALUES ("Engineering"),
+       ("Sales"),
+       ("Marketing");
+
+
+       
+INSERT INTO role (title, salary)
+VALUES ("Software Engineer", 90000),
+       ("Sales agent", 80000),
+       ("Manager", 70000);
+
+INSERT INTO employee (first_name, last_name,manager_id)
+VALUES ("Bob", "Peet", 90),
+        
